6e5ef57bfb89b2005d79dba61b58341c280f4d3d
sputhan <sputhan@swaggerhub.com>
2020-12-14 09:54:46 +0000

    API definition transferred by SwaggerHub

--- platform-api-spec/swagger_server/__main__.py
+++ platform-api-spec/swagger_server/__main__.py
@@ -5,16 +5,12 @@ import connexion
 from swagger_server import encoder
 
 
-#def main():
-app = connexion.App(__name__, specification_dir='./swagger/')
-application = app.app
-app.app.json_encoder = encoder.JSONEncoder
-app.add_api('swagger.yaml', arguments={'title': 'finest-platform-services'})
-#app.run(port=8080)
-
-
+def main():
+    app = connexion.App(__name__, specification_dir='./swagger/')
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('swagger.yaml', arguments={'title': 'finest-platform-services'})
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
-    #main()
-    pass
+    main()
